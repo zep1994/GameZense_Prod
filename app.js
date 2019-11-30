06499f963481dca82ff1c0f36971b49f47996beb
zep1994 <snaxbags@protonmail.com>
2019-11-30 13:44:14 -0500

    begin setting up form

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const http = require('http')
 http.createServer((req, res) => {
     const url = req.url 
     if (url === '/') {
-        res.end(`Server is listening on port 3000`)
+        res.write('<html>')
+        res.write('<body></body><form action="/post" method="POST"><input type="text" name="post" /><input type="submit" /></form><body')
+        res.write('</html')
+        return res.end()
     }
 }).listen(3000)
 
